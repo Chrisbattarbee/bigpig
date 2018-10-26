2a80f705b22ae6ddaf8275f4765009c7ead68608
Chris Battarbee <chrisbattarbee3@gmail.com>
2018-10-26 14:49:27 +0100

    Initial co-ordinator commit

---
+++ co-ordinator/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import com.romix.scala.collection.concurrent.TrieMap;
+
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map myMap = new TrieMap<Integer, Integer>();
+    }
+}
