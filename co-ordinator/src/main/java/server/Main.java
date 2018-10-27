2f14a1ae8367a10e89b6cbbd835507d9c0c8ace9
Chris Battarbee <chrisbattarbee3@gmail.com>
2018-10-27 17:19:09 +0100

    Fix bug of not loading in serialized data to deserialize in utils

--- co-ordinator/src/main/java/server/Main.java
+++ co-ordinator/src/main/java/server/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server();
         server.start();
+        System.out.println("Started Server");
         server.blockUntilShutDown();
     }
 }
