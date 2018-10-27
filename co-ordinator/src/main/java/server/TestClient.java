80ebc478c2be026313c7a8a70e1e429d4d373ab2
Chris Battarbee <chrisbattarbee3@gmail.com>
2018-10-27 17:05:00 +0100

    Add more diagnostic code

--- co-ordinator/src/main/java/server/TestClient.java
+++ co-ordinator/src/main/java/server/TestClient.java
@@ -8,6 +8,10 @@ import java.util.Map;
 public class TestClient {
     public static void main(String[] args) {
         Map map = new CoordinatorCTrie("localhost", ByteStringManipulation.PORT_NUMBER);
-        System.out.println(map.size());
+        System.out.println("size: " + map.size());
+
+        System.out.println("Putting 1, 5 into the map");
+        map.put(1, 5);
+        System.out.println("size: " + map.size());
     }
 }
