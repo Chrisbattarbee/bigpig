bc0753cd9685d705da86b74b653bc5b6bc1651e0
Ivan Kotegov <ik716@ic.ac.uk>
2018-10-28 18:16:50 +0000

    Fix JDart Docker image

--- symbex/entrypoint.sh
+++ symbex/entrypoint.sh
@@ -1,11 +1,9 @@
-#! bin/bash
+#! /bin/bash
 
-cd /usr/lib/jpf/
+cd /root/jpf/jdart
 javac -d /mnt/build /mnt/*.java 
 # ./jpf-core/bin/jpf jpf-core/Std.jpf
 
-#java -XshowSettings:properties 
-
-cd jdart
 ./../jpf-core/bin/jpf src/examples/features/simple/test_baz.jpf
+./../jpf-core/bin/jpf ../Std.jpf
 
