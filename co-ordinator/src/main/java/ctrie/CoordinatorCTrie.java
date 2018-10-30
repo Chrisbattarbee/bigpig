df6c2f0eb8f3aa93e5c92cb094148a620761dab7
Nik <ns4516@ic.ac.uk>
2018-10-30 13:54:59 +0000

    Merge async and coord classes

--- co-ordinator/src/main/java/ctrie/CoordinatorCTrie.java
+++ co-ordinator/src/main/java/ctrie/CoordinatorCTrie.java
@@ -1,5 +1,6 @@
 package ctrie;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -8,6 +9,10 @@ import java.io.*;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static utils.ByteStringManipulation.*;
 
@@ -15,6 +20,8 @@ public class CoordinatorCTrie<K extends Serializable, V extends Serializable> im
 
     private final ManagedChannel channel;
     private final CTrieServiceGrpc.CTrieServiceBlockingStub blockingStub;
+    private final CTrieServiceGrpc.CTrieServiceFutureStub futureStub;
+
 
     public CoordinatorCTrie() {
         this(ManagedChannelBuilder.forAddress(HOST_NAME, PORT_NUMBER).usePlaintext());
@@ -28,6 +35,7 @@ public class CoordinatorCTrie<K extends Serializable, V extends Serializable> im
         channel = channelBuilder.build();
 
         blockingStub = CTrieServiceGrpc.newBlockingStub(channel);
+        futureStub   =  CTrieServiceGrpc.newFutureStub(channel);
     }
 
     @Override
@@ -69,6 +77,51 @@ public class CoordinatorCTrie<K extends Serializable, V extends Serializable> im
         ByteString serializedValue = objectToByteString(value);
         PutRequest putRequest = PutRequest.newBuilder().setSerializedKeyObject(serializedKey).setSerializedValueObject(serializedValue).build();
         return (V) byteStringToObject(blockingStub.put(putRequest).getSerializedValueObject());
+    }
+
+    //Future wrapper for future put response - to convert byte string to object
+    public class FutureObject<V extends Serializable> implements Future {
+
+        private ListenableFuture<PutResponse> response;
+
+        public FutureObject(ListenableFuture<PutResponse> response) {
+            this.response = response;
+        }
+
+        @Override
+        public boolean cancel(boolean mayInterruptIfRunning) {
+            return response.cancel(mayInterruptIfRunning);
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return response.isCancelled();
+        }
+
+        @Override
+        public boolean isDone() {
+            return response.isDone();
+        }
+
+        @Override
+        public Object get() throws InterruptedException, ExecutionException {
+            return (V) byteStringToObject(response.get().getSerializedValueObject());
+        }
+
+        @Override
+        public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+            return (V) byteStringToObject(response.get(timeout, unit).getSerializedValueObject());
+        }
+    }
+
+    public Future<V> asyncPut(K key, V value) {
+        ByteString serializedKey = objectToByteString(key);
+        ByteString serializedValue = objectToByteString(value);
+        PutRequest putRequest = PutRequest.newBuilder().setSerializedKeyObject(serializedKey).setSerializedValueObject(serializedValue).build();
+
+        ListenableFuture<PutResponse> response = futureStub.put(putRequest);
+        return new FutureObject(response);
+
     }
 
     @Override
