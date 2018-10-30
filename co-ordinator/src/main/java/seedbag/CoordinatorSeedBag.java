f02cbc681d4b3403776036064380494c1571f8db
Chris Battarbee <Chrisbattarbee@users.noreply.github.com>
2018-10-30 18:28:45 +0000

    Co ordinator (#18)
    * Initial co-ordinator commit
    
    * Add grpc dependencies
    
    * Add proto file
    
    * Add protos for the CTrie
    
    * Move file to be called CTrie
    
    * Add new proto genned file
    
    * Change file name
    
    * Gen more stub code
    
    * Remove gradle genning protos
    
    * Build GRPC client map
    
    * Remove white space
    
    * Implement server, test client and a couple of server methods
    
    * Implement contains key and contains value
    
    * Implement get
    
    * Implement put
    
    * Add more diagnostic code
    
    * Fix bug of not loading in serialized data to deserialize in utils
    
    * Implement remove
    
    * Implement putAll and clear
    
    * Implement keySet
    
    * Implement values and entryset
    
    * Whitespace cleanup
    
    * Add docker file to run grpc server
    
    * whitespace
    
    * Initial co-ordinator commit
    
    * Add grpc dependencies
    
    * Add proto file
    
    * Add protos for the CTrie
    
    * Move file to be called CTrie
    
    * Add new proto genned file
    
    * Change file name
    
    * Gen more stub code
    
    * Remove gradle genning protos
    
    * Build GRPC client map
    
    * Remove white space
    
    * Implement server, test client and a couple of server methods
    
    * Implement contains key and contains value
    
    * Implement get
    
    * Implement put
    
    * Add more diagnostic code
    
    * Fix bug of not loading in serialized data to deserialize in utils
    
    * Implement remove
    
    * Implement putAll and clear
    
    * Implement keySet
    
    * Implement values and entryset
    
    * Whitespace cleanup
    
    * Add docker file to run grpc server
    
    * whitespace
    
    * Remove dead code
    
    * Add snapshot to CTrie
    
    * Async interface (#15)
    
    * Create initial Async class
    
    * Add async put
    
    * Merge async and coord classes
    
    * Add async get
    
    * Add async tests to Testclient
    
    * Add async get tests to Testclient
    
    * Update the interface
    
    * Code cleanup and async putAll test
    
    * Fix tests and remove whitespace
    
    * Seed bag implementation (#11)
    
    * Create seedbag proto
    
    * Fix missing proto params
    
    * Generate java files
    
    * Implement the client
    
    * Refactor server architecture
    
    * Implement seedbag
    
    * Add more tests
    
    * Use underlying concurrent data structure
    
    * Update comment

---
+++ co-ordinator/src/main/java/seedbag/CoordinatorSeedBag.java
@@ -0,0 +1,139 @@
+package seedbag;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+
+import static utils.ByteStringManipulation.*;
+
+public class CoordinatorSeedBag<E extends Serializable> implements Queue<E> {
+
+    private final ManagedChannel channel;
+    private final SeedBagServiceGrpc.SeedBagServiceBlockingStub blockingStub;
+
+    public CoordinatorSeedBag() {
+        this(ManagedChannelBuilder.forAddress(HOST_NAME, PORT_NUMBER).usePlaintext());
+    }
+
+    public CoordinatorSeedBag(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    }
+
+    public CoordinatorSeedBag(ManagedChannelBuilder<?> channelBuilder) {
+        channel = channelBuilder.build();
+
+        blockingStub = SeedBagServiceGrpc.newBlockingStub(channel);
+    }
+
+    @Override
+    public int size() {
+        SizeRequest sizeRequest = SizeRequest.newBuilder().build();
+        return blockingStub.size(sizeRequest).getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        IsEmptyRequest isEmptyRequest = IsEmptyRequest.newBuilder().build();
+        return blockingStub.isEmpty(isEmptyRequest).getIsEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        ContainsRequest containsRequest = ContainsRequest.newBuilder().setSerializedObject(objectToByteString(o)).build();
+        return blockingStub.contains(containsRequest).getDoesContain();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        IteratorRequest iteratorRequest = IteratorRequest.newBuilder().build();
+        return (Iterator<E>) byteStringToObject(blockingStub.iterator(iteratorRequest).getSerializedIterator());
+    }
+
+    @Override
+    public Object[] toArray() {
+        ToArrayRequest toArrayRequest = ToArrayRequest.newBuilder().build();
+        return (Object[]) byteStringToObject(blockingStub.toArray(toArrayRequest).getSerializedArrayObject());
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return (T[]) toArray();
+    }
+
+    @Override
+    public boolean add(E e) {
+        AddRequest addRequest = AddRequest.newBuilder().setSerializedObject(objectToByteString(e)).build();
+        return blockingStub.add(addRequest).getAdded();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        RemoveRequest removeRequest = RemoveRequest.newBuilder().setSerializedObject(objectToByteString(o)).build();
+        return blockingStub.remove(removeRequest).getRemoved();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        ContainsAllRequest containsAllRequest = ContainsAllRequest.newBuilder().setSerializedCollection(objectToByteString(c)).build();
+        return blockingStub.containsAll(containsAllRequest).getContainsAll();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        AddAllRequest addAllRequest = AddAllRequest.newBuilder().setSerializedCollection(objectToByteString(c)).build();
+        return blockingStub.addAll(addAllRequest).getAllAdded();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        RemoveAllRequest removeAllRequest = RemoveAllRequest.newBuilder().setSerializedCollection(objectToByteString(c)).build();
+        return blockingStub.removeAll(removeAllRequest).getAllRemoved();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        RetainAllRequest retainAllRequest = RetainAllRequest.newBuilder().setSerializedCollection(objectToByteString(c)).build();
+        return blockingStub.retainAll(retainAllRequest).getAllRetained();
+    }
+
+    @Override
+    public void clear() {
+        ClearRequest clearRequest = ClearRequest.newBuilder().build();
+        blockingStub.clear(clearRequest);
+    }
+
+    @Override
+    public boolean offer(E e) {
+        OfferRequest offerRequest = OfferRequest.newBuilder().setSerializedObject(objectToByteString(e)).build();
+        return blockingStub.offer(offerRequest).getOffered();
+    }
+
+    @Override
+    public E remove() {
+        RemoveNoArgsRequest removeRequest = RemoveNoArgsRequest.newBuilder().build();
+        return (E) byteStringToObject(blockingStub.removeNoArgs(removeRequest).getSerializedObject());
+    }
+
+    @Override
+    public E poll() {
+        PollRequest pollRequest = PollRequest.newBuilder().build();
+        return (E) byteStringToObject(blockingStub.poll(pollRequest).getSerializedObject());
+    }
+
+    @Override
+    public E element() {
+        ElementRequest elementRequest = ElementRequest.newBuilder().build();
+        return (E) byteStringToObject(blockingStub.element(elementRequest).getSerializedObject());
+    }
+
+    @Override
+    public E peek() {
+        PeekRequest peekRequest = PeekRequest.newBuilder().build();
+        return (E) byteStringToObject(blockingStub.peek(peekRequest).getSerializedObject());
+    }
+}
---
+++ co-ordinator/src/main/java/server/SeedBagService.java
@@ -0,0 +1,144 @@
+package server;
+
+import io.grpc.stub.StreamObserver;
+import seedbag.*;
+
+import java.util.Collection;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import static utils.ByteStringManipulation.*;
+
+public class SeedBagService extends SeedBagServiceGrpc.SeedBagServiceImplBase {
+
+    private Queue<Object> queue;
+
+    public SeedBagService() {
+        queue = new ConcurrentLinkedDeque<>();
+    }
+
+    @Override
+    public void size(SizeRequest request, StreamObserver<SizeResponse> responseObserver) {
+        responseObserver.onNext(SizeResponse.newBuilder().setSize(queue.size()).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void isEmpty(IsEmptyRequest request, StreamObserver<IsEmptyResponse> responseObserver) {
+        responseObserver.onNext(IsEmptyResponse.newBuilder().setIsEmpty(queue.isEmpty()).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void contains(ContainsRequest request, StreamObserver<ContainsResponse> responseObserver) {
+        responseObserver.onNext(ContainsResponse.newBuilder().setDoesContain(queue.contains(byteStringToObject(request.getSerializedObject()))).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    // TODO(Chris) Implement this whenever I have the energy to think of an optimal solution
+    public void iterator(IteratorRequest request, StreamObserver<IteratorResponse> responseObserver) {
+        super.iterator(request, responseObserver);
+    }
+
+    @Override
+    public void toArray(ToArrayRequest request, StreamObserver<ToArrayResponse> responseObserver) {
+        responseObserver.onNext(ToArrayResponse.newBuilder().setSerializedArrayObject(objectToByteString(queue.toArray())).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void add(AddRequest request, StreamObserver<AddResponse> responseObserver) {
+        boolean result = queue.add(byteStringToObject(request.getSerializedObject()));
+
+        responseObserver.onNext(AddResponse.newBuilder().setAdded(result).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void remove(RemoveRequest request, StreamObserver<RemoveResponse> responseObserver) {
+        boolean result = queue.remove(byteStringToObject(request.getSerializedObject()));
+
+        responseObserver.onNext(RemoveResponse.newBuilder().setRemoved(result).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void containsAll(ContainsAllRequest request, StreamObserver<ContainsAllResponse> responseObserver) {
+        boolean result = queue.containsAll((Collection<?>) byteStringToObject(request.getSerializedCollection()));
+
+        responseObserver.onNext(ContainsAllResponse.newBuilder().setContainsAll(result).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void addAll(AddAllRequest request, StreamObserver<AddAllResponse> responseObserver) {
+        boolean result = queue.addAll((Collection<?>) byteStringToObject(request.getSerializedCollection()));
+
+        responseObserver.onNext(AddAllResponse.newBuilder().setAllAdded(result).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void retainAll(RetainAllRequest request, StreamObserver<RetainAllResponse> responseObserver) {
+        boolean result = queue.retainAll((Collection<?>) byteStringToObject(request.getSerializedCollection()));
+
+        responseObserver.onNext(RetainAllResponse.newBuilder().setAllRetained(result).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void removeAll(RemoveAllRequest request, StreamObserver<RemoveAllResponse> responseObserver) {
+        boolean result = queue.removeAll((Collection<?>) byteStringToObject(request.getSerializedCollection()));
+
+        responseObserver.onNext(RemoveAllResponse.newBuilder().setAllRemoved(result).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void clear(ClearRequest request, StreamObserver<ClearResponse> responseObserver) {
+        queue.clear();
+
+        responseObserver.onNext(ClearResponse.newBuilder().build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void offer(OfferRequest request, StreamObserver<OfferResponse> responseObserver) {
+        boolean result = queue.offer(byteStringToObject(request.getSerializedObject()));
+
+        responseObserver.onNext(OfferResponse.newBuilder().setOffered(result).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void removeNoArgs(RemoveNoArgsRequest request, StreamObserver<RemoveNoArgsResponse> responseObserver) {
+        Object o = queue.remove();
+
+        responseObserver.onNext(RemoveNoArgsResponse.newBuilder().setSerializedObject(objectToByteString(o)).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void poll(PollRequest request, StreamObserver<PollResponse> responseObserver) {
+        Object o = queue.poll();
+
+        responseObserver.onNext(PollResponse.newBuilder().setSerializedObject(objectToByteString(o)).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void element(ElementRequest request, StreamObserver<ElementResponse> responseObserver) {
+        Object o = queue.element();
+
+        responseObserver.onNext(ElementResponse.newBuilder().setSerializedObject(objectToByteString(o)).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void peek(PeekRequest request, StreamObserver<PeekResponse> responseObserver) {
+        Object o = queue.peek();
+        responseObserver.onNext(PeekResponse.newBuilder().setSerializedObject(objectToByteString(o)).build());
+        responseObserver.onCompleted();
+    }
+}
